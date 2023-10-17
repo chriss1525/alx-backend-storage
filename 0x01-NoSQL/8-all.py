a41f11e033ca57344b53bafef5c42d42c0c96cc1
chriss1525 <okothchris15@gmail.com>
2023-10-18 00:58:15 +0300

    using python to list all the documents found in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# list all documents in a collection
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    """lists all documents in a collection"""
+    return mongo_collection.find()
