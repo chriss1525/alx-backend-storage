52d1f5f8206e866e97931a1efce2cefdb13a6cf8
chriss1525 <okothchris15@gmail.com>
2023-10-19 02:00:32 +0300

    storing the history of inputs and outputs in redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,24 @@ import redis
 from functools import wraps
 
 
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        name = method.__qualname__
+        input_key = name + ":inputs"
+        output_key = name + ":outputs"
+
+        input_value = str(args)
+        self._redis.rpush(input_key, input_value)
+
+        output_value = str(method(self, *args, **kwargs))
+
+        self._redis.rpush(output_key, output_value)
+
+        return output_value
+    return wrapper
+
+
 def count_calls(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
@@ -21,6 +39,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(self._redis.incr("count"))
         self._redis.set(key, data)
